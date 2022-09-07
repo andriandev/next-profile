06f37cd3b5fa8b6d4c7ea8e39ec3706178f59de0
Andrian Dev <andrianpoint@gmail.com>
2022-09-07 19:19:59 +0700

    Custom footer

--- app/layout/footer.jsx
+++ app/layout/footer.jsx
@@ -1,5 +1,18 @@
 function Footer() {
-  return <footer className="container">Footer</footer>;
+  return (
+    <footer className="bg-light">
+      <p className="text-center text-muted my-3">
+        Copyright ©{new Date().getFullYear() + ' '}
+        <a
+          href="https://andriandev.my.id"
+          target="_blank"
+          className="text-muted text-decoration-none"
+        >
+          AndrianDev
+        </a>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
