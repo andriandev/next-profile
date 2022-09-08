fe247173ee75aef3887d132126a0f5310ea9e9a1
Andrian Dev <andrianpoint@gmail.com>
2022-09-08 17:03:43 +0700

    Next profile second commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import MetaHead from '@/components/shared/meta-head';
 import Card from '@/components/shared/card';
 
@@ -8,8 +9,37 @@ function Home() {
         title="AndrianDev - Web Developer"
         description="Andriandev merupakan website portofolio pribadi dan tempat menyimpan berbagai projek"
       />
-      <Card classCard="mt-3">
-        <h1>Home</h1>
+      <Card classCard="my-3">
+        <div className="row">
+          <div className="col-sm-12 col-md-4 col-lg-3 text-center">
+            <Image
+              width="220"
+              height="220"
+              src="/assets/img/profile.jpg"
+              className="rounded mx-auto d-block img-thumbnail"
+            />
+          </div>
+          <div className="col-sm-12 col-md-8 col-lg-9 my-2">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <b>Nama:</b> Andrian Adi Saputra
+              </li>
+              <li className="list-group-item">
+                <b>Alamat:</b> Pringsewu, Lampung, Indonesia
+              </li>
+              <li className="list-group-item">
+                <b>Umur:</b> 22 Tahun
+              </li>
+              <li className="list-group-item">
+                <b>Gender:</b> Pria
+              </li>
+              <li className="list-group-item">
+                <b>Keahlian:</b> HTML, CSS, Javascript, PHP, Node Js, React Js,
+                Next Js, Git.
+              </li>
+            </ul>
+          </div>
+        </div>
       </Card>
     </>
   );
